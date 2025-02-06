3e2aa19bb3b0cb616b2d84984b7522889c09159b
Tiago Teles <teletiago1@hotmail.com>
2025-02-06 11:52:37 +0000

    Improve example code

--- src/QBlade/main.py
+++ src/QBlade/main.py
@@ -1,66 +1,57 @@
 import os
+import QBladeLib
 import sys
-# from ctypes import *
-from QBladeLibrary import QBladeLibrary
-dll_name = 'bin/QBlade/'
-turbine_path = 'data/QBlade/DTU_10MW/'
-turbine_name = 'DTU_10MW'
 
-# On Windows systems, we update the PATH environment variable to include the QBlade directory.
-# This ensures that the required SSL libraries (e.g., libssl and libcrypto) are properly located and loaded.
-# If experiencing issues with this DLL in a Windows Python environment see:
-# https://docs.qblade.org/src/license/license_files.html#resolving-openssl-issues-on-windows
-if os.name == 'nt':  # 'nt' indicates Windows
-    os.environ["PATH"] = os.path.abspath(dll_name) + ";" + os.environ.get("PATH", "")
+# QBlade configuration
+DLL_PATH = "bin/QBlade/"    # DLL path
+DLL_NAME = "QBlade.dll"     # DLL name
+CL_DEVICE = 1               # OpenCL device
+GROUP_SIZE = 32             # OpenCL work-group size
 
-# Search the directory below for library files matching the pattern QBlade*.dll or QBlade*.so
+# Simulation specification
+TURBINE_PATH = "data/QBlade/DTU_10MW/"  # Turbine path
+TURBINE_NAME = "DTU_10MW.sim"           # Turbine name
+N_TIMESTEP = 500                        # Number of timesteps
+
+# Search DLL_PATH for files matching DLL_NAME
 dll_files = []
 
-for f in os.listdir(dll_name):
-    if 'QBlade' in f and ('.dll' in f or '.so' in f):
+for f in os.listdir(DLL_PATH):
+    if f == DLL_NAME:
         dll_files.append(f)
 
-# Check if any matching files are found
-if not dll_files:
-    print('No matching QBlade*.dll or QBlade*.so files found in the specified directory:', os.path.abspath(dll_name))
-    sys.exit(1)  # Exit the script with a non-zero status to indicate an error
+# Ensure a matching file is found
+if dll_files:
+    print(f"Using shared library file: {os.path.join(DLL_PATH, DLL_NAME)}!")
+else:
+    print(f"No shared library file named {DLL_NAME} was found in {DLL_PATH}!")
+    sys.exit(1)
 
-# Use the first matching file
-dll_file_path = os.path.join(dll_name, dll_files[0])
+# Create an object that contains the API
+QBlade = QBladeLib.QBlade(os.path.join(DLL_PATH, DLL_NAME))
 
-        except Exception as e:
-            raise RuntimeError(f"Could not load the library at {self.lib_path}: {e}") from e
+# Create a QBlade instance from the library
+QBlade.createInstance(CL_DEVICE, GROUP_SIZE)
 
-# Create an object of the class 'QBladeLibrary' that contains the API
-QBlade = QBladeLibrary(dll_file_path)
+# Load a simulation definition file
+QBlade.loadSimDefinition(os.path.join(TURBINE_PATH, TURBINE_NAME).encode("utf-8"))
 
-# Creation of a QBlade instance from the library
-QBlade.createInstance(1, 32)
-
-# Loading a project or sim-file, in this case the DTU_10MW_Demo project or simulation definition file
-QBlade.loadSimDefinition(os.path.join(turbine_path, turbine_name, '.sim'))
-
-# Initializing the sim and ramp-up phase, call before starting the simulation loop
+# Initializing the simulation
 QBlade.initializeSimulation()
 
-    def unload_library(self):
-        """Close the QBlade instance if it exists."""
+# Run the simulation
+for i in range(N_TIMESTEP):
 
-        # Close QBlade instance
-        try:
-            self.closeInstance()
-            print("QBlade instance closed.")
-
-    #advance the simulation
+    # Advance the simulation one timestep
     success = QBlade.advanceTurbineSimulation()
 
-    # Check if the simulation step was successful
-    if not success:  # If success is False, exit the loop
-        print(f"Simulation failed at timestep {i}. Exiting loop.")
+    # Ensure the simulation step was successful
+    if not success:
+        print(f"Simulation failed at timestep {i}!")
         break
 
-# Storing the simulation results in QBlade ASCII format in the file NREL_5MW_Sample_results.txt
-QBlade.exportResults(0, turbine_path, turbine_name, b"")
+# Store the simulation results
+QBlade.exportResults(0, TURBINE_PATH.encode("utf-8"), os.path.splitext(TURBINE_NAME)[0].encode("utf-8"), b"")
 
-# Unloading the qblade library
-QBlade.unload_library() 
+# Unload the QBlade library
+QBlade.unload_library()
