4325b644a7ecce0bee4401ea282dd8a140a37902
Tiago Teles <teletiago1@hotmail.com>
2025-02-07 13:50:49 +0000

    Add convergence checking

--- src/XFoil/xfoil.py
+++ src/XFoil/xfoil.py
@@ -34,15 +34,16 @@ class XFoil:
             print(f"No airfoil file found at {path}")
             sys.exit(1)
 
-    def run(self, re, mach, alpha, path):
+    def run(self, re, mach, alpha, path, max_iter=1000):
         """
         Run XFoil at a given Re, M, and Alpha.
-        
+
         Keyword arguments:
             re -- the Reynolds number
             mach -- the Mach number
             alpha -- the angle of attack
             path -- the path to the output file
+            max_iter -- the maximum number of iterations (default 100)
 
         Returns:
             stdout -- the standard output stream
@@ -55,9 +56,29 @@ class XFoil:
 
         # Queue XFoil commands
         self.process.stdin.write("OPER\n")
+        self.process.stdin.write(f"ITER {max_iter}\n")
         self.process.stdin.write(f"Visc {re}\n")
         self.process.stdin.write(f"Mach {mach}\n")
         self.process.stdin.write(f"ALFA {alpha}\n")
         self.process.stdin.write(f"DUMP {path}\n")
+        self.process.stdin.write("\n")
+        self.process.stdin.write("QUIT\n")
 
-        return self.process.communicate()
+        # Run XFoil commands
+        stdout, stderr = self.process.communicate()
+
+        # Check for convergence problems
+        if "Type \"!\" to continue iterating" in stdout:
+            print("VISCAL: Convergence failed")
+            os.remove(path)
+            # sys.exit(1)
+
+        return stdout, stderr
+
+for file in os.listdir("data/QBlade/DTU_10MW/Aero/Airfoils"):
+
+    filepath = os.path.join("data/QBlade/DTU_10MW/Aero/Airfoils", file)
+
+    test = XFoil("bin/XFoil/XFoil.exe")
+    test.load_airfoil(filepath)
+    test.run(1E6, 0.2, 0.0, filepath.replace(".afl", ".dat").replace("Airfoils", "Boundaries"))
