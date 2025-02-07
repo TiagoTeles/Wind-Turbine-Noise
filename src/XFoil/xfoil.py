fd6ef24f4821d59f869db26348c50ff5fe18945b
Tiago Teles <teletiago1@hotmail.com>
2025-02-07 17:55:15 +0000

    Improve documentation

--- src/XFoil/xfoil.py
+++ src/XFoil/xfoil.py
@@ -1,49 +1,86 @@
+""" 
+Author:   T. Moreira da Fonte Fonseca Teles
+Email:    tmoreiradafont@tudelft.nl
+Date:     2025-02-07
+License:  GNU GPL 3.0
+
+Run the XFoil executable.
+
+Classes:
+    XFoil
+
+Functions:
+    None
+
+Exceptions:
+    None
+"""
+
 import os
 import subprocess as sp
 import sys
 
 
 class XFoil:
-    """A class to interact with XFoil."""
+    """
+    A class to run the XFoil executable.
+    
+    Methods:
+        __init__ -- spawn an XFoil process
+        load_airfoil -- load an airfoil from a file
+        run -- run XFoil at a given Re, M, and Alpha
+    
+    Attributes:
+        path : str -- the path to the XFoil executable
+        process : subprocess.Popen -- the XFoil process
+    """
 
     def __init__(self, path):
         """
         Spawn an XFoil process.
 
-        Keyword arguments:
-            path -- the path to the XFoil executable
+        Parameters:
+            path : str -- the path to the XFoil executable
+
+        Returns:
+            None
         """
+
+        self.path = path
 
         if os.path.isfile(path):
             self.process = sp.Popen(path, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, text=True)
         else:
-            print(f"No executable file found at {path}")
+            print(f"No executable found at {path}!")
             sys.exit(1)
 
     def load_airfoil(self, path):
         """
         Load an airfoil from a file.
 
-        Keyword arguments:
-            path -- the path to the airfoil file
+        Parameters:
+            path : str -- the path to the airfoil file
+
+        Returns:
+            None
         """
 
         if os.path.isfile(path):
             self.process.stdin.write(f"LOAD {path}\n")
         else:
-            print(f"No airfoil file found at {path}")
+            print(f"No airfoil file found at {path}!")
             sys.exit(1)
 
-    def run(self, re, mach, alpha, path, max_iter=1000):
+    def run(self, re, mach, alpha, path, it=100):
         """
         Run XFoil at a given Re, M, and Alpha.
 
-        Keyword arguments:
-            re -- the Reynolds number
-            mach -- the Mach number
-            alpha -- the angle of attack
-            path -- the path to the output file
-            max_iter -- the maximum number of iterations (default 100)
+        Parameters:
+            re : float -- the Reynolds number
+            mach : float -- the Mach number
+            alpha : float -- the angle of attack
+            path : str -- the path to the output file
+            it : int -- the maximum number of iterations (default 100)
 
         Returns:
             stdout -- the standard output stream
@@ -56,29 +93,21 @@ class XFoil:
 
         # Queue XFoil commands
         self.process.stdin.write("OPER\n")
+        self.process.stdin.write(f"ITER {it}\n")
         self.process.stdin.write(f"Visc {re}\n")
         self.process.stdin.write(f"Mach {mach}\n")
-        self.process.stdin.write(f"ITER {max_iter}\n")
         self.process.stdin.write(f"ALFA {alpha}\n")
         self.process.stdin.write(f"DUMP {path}\n")
         self.process.stdin.write("\n")
         self.process.stdin.write("QUIT\n")
 
-        # Run XFoil commands
+        # Run XFoil
         stdout, stderr = self.process.communicate()
 
-        # Check for convergence problems
+        # Check for failed convergence
         if "Type \"!\" to continue iterating" in stdout:
-            print("VISCAL: Convergence failed")
+            print("XFoil convergence failed")
             os.remove(path)
-            # sys.exit(1)
+            sys.exit(1)
 
         return stdout, stderr
-
-for file in os.listdir("data/QBlade/DTU_10MW/Aero/Airfoils"):
-
-    filepath = os.path.join("data/QBlade/DTU_10MW/Aero/Airfoils", file)
-
-    test = XFoil("bin/XFoil/XFoil.exe")
-    test.load_airfoil(filepath)
-    test.run(1E6, 0.2, 0.0, filepath.replace(".afl", ".dat").replace("Airfoils", "Boundaries"))
