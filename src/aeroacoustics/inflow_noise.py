24153c1ed008f0f7406f3fa3c350c72d030ff283
Tiago Teles <teletiago1@hotmail.com>
2025-02-19 15:29:07 +0000

    Add .py aeroacoustic files

---
+++ src/aeroacoustics/inflow_noise.py
@@ -0,0 +1,19 @@
+"""
+Author:   T. Moreira da Fonte Fonseca Teles
+Email:    tmoreiradafont@tudelft.nl
+Date:     2025-02-19
+License:  GNU GPL 3.0
+
+Calculate the inflow noise.
+
+Classes:
+    None
+
+Functions:
+    TBD
+
+Exceptions:
+    None
+"""
+
+import numpy as np
