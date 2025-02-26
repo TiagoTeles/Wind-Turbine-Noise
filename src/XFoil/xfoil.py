8163dfea1905fa4270da2360d5fcd5e782674573
Tiago Teles <teletiago1@hotmail.com>
2025-02-26 15:45:15 +0000

    Add ability to read output file

--- src/XFoil/xfoil.py
+++ src/XFoil/xfoil.py
@@ -1,7 +1,7 @@
 """ 
 Author:   T. Moreira da Fonte Fonseca Teles
 Email:    tmoreiradafont@tudelft.nl
-Date:     2025-02-17
+Date:     2025-02-26
 License:  GNU GPL 3.0
 
 Run the XFoil executable.
@@ -20,21 +20,20 @@ import os
 import subprocess as sp
 import sys
 
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 class XFoil:
     """
     A class to run the XFoil executable.
-    
+
     Methods:
         __init__ -- spawn an XFoil process
-        load_airfoil -- load an airfoil from a file
         run -- run XFoil at a given Re, M, and Alpha
-    
+
     Attributes:
-        airfoil : str -- airfoil file path
-        path : str -- path to the XFoil executable
         process : subprocess.Popen -- XFoil process
     """
 
@@ -49,52 +48,37 @@ class XFoil:
             None
         """
 
-        self.path = path
-        self.airfoil = None
-
         if os.path.isfile(path):
             self.process = sp.Popen(path, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, text=True)
         else:
             print(f"No executable found at {path}!")
             sys.exit(1)
 
-    def load_airfoil(self, path):
+    def run(self, path, re, mach, alpha, xtr_top=1.0, xtr_bot=1.0, n_crit=9.0, it=10):
         """
-        Load an airfoil from a file.
+        Run XFoil at a given Re, M, and Alpha.
 
         Arguments:
-            path : str -- path to the airfoil file
+            path : str -- airfoil file path
+            re : float -- Reynolds number, [-]
+            mach : float -- Mach number, [-]
+            alpha : float -- angle of attack, [rad]
+            xtr_top : float -- transition point on the top surface (default 1.0), [-]
+            xtr_bot : float -- transition point on the bottom surface (default 1.0), [-]
+            n_crit : float -- critical amplification factor (default 9.0), [-]
+            it : int -- maximum number of iterations (default 10), [-]
 
         Returns:
-            None
+            boundary_layer_top : pandas.DataFrame -- boundary layer data on the top surface
+            boundary_layer_bot : pandas.DataFrame -- boundary layer data on the bottom surface
         """
 
-        self.airfoil = path
-
+        # Load airfoil file
         if os.path.isfile(path):
             self.process.stdin.write(f"LOAD {path}\n")
         else:
             print(f"No airfoil file found at {path}!")
             sys.exit(1)
-
-    def run(self, re, mach, alpha, path, xtr_top=1.0, xtr_bot=1.0, n_crit=9.0, it=10):
-        """
-        Run XFoil at a given Re, M, and Alpha.
-
-        Arguments:
-            re : float -- Reynolds number, [-]
-            mach : float -- Mach number, [-]
-            alpha : float -- angle of attack, [rad]
-            path : str -- path to the output file
-            xtr_top : float -- transition point on the top surface (default 1.0), [-]
-            xtr_bot : float -- transition point on the bottom surface (default 1.0), [-]
-            n_crit : float -- critical amplification factor (default 9.0), [-]
-            it : int -- maximum number of iterations (default 100), [-]
-
-        Returns:
-            stdout -- the standard output stream
-            stderr -- the standard error stream
-        """
 
         # Set Re, Ma, and ITER in OPER environment
         self.process.stdin.write("OPER\n")
@@ -112,29 +96,69 @@ class XFoil:
         self.process.stdin.write(f"Alfa {np.degrees(alpha)}\n")
 
         # Save results to a file
-        if not os.path.exists(os.path.dirname(path)):
-            os.makedirs(os.path.dirname(path))
+        output_path = os.path.join("tmp\\xfoil", os.path.basename(path))
 
-        self.process.stdin.write(f"DUMP {path}\n")
+        if not os.path.exists(os.path.dirname(output_path)):
+            os.makedirs(os.path.dirname(output_path))
+
+        self.process.stdin.write(f"DUMP {output_path}\n")
         self.process.stdin.write("\n")
 
         # Quit XFoil
         self.process.stdin.write("QUIT\n")
 
         # Run XFoil commands
-        stdout, stderr = self.process.communicate()
+        stdout, _ = self.process.communicate()
 
         # Check for failed convergence
         if "Type \"!\" to continue iterating" in stdout:
             print("XFoil convergence failed!")
-            print(f"Airfoil: {self.airfoil}, Re: {re}, Ma: {mach}, Alpha: {alpha}")
-            os.remove(path)
+            print(f"Airfoil: {path}, Re: {re}, Ma: {mach}, Alpha: {alpha}")
             sys.exit(1)
 
-        return stdout, stderr
+        # Read output file
+        data = pd.read_csv(output_path, sep=r"\s+", skiprows=1,
+                           names=["s/c", "x/c", "y/c", "U_e/U", "delta_star", "theta", "C_f", "H"])
+
+        # Remove output file
+        os.remove(output_path)
+
+        # Filter and sort boundary layer data
+        le_index = data.idxmin()["x/c"]
+
+        boundary_layer_top = data[data["x/c"] <= 1.0].iloc[:le_index+1][::-1]
+        boundary_layer_bot = data[data["x/c"] <= 1.0].iloc[le_index:]
+
+        return boundary_layer_top, boundary_layer_bot
 
 # if __name__ == "__main__":
 
-#     xfoil = XFoil("bin\\XFoil\\xfoil.exe")
-#     xfoil.load_airfoil("data\\turbines\\DTU_10MW\\Aero\\Airfoils\\FFA_W3_241.afl")
-#     stdout, stderr = xfoil.run(1E6, 0.2, 0, ".\\FFA_W3_241_1E6_0.2_0.dat")
+#     XFOIL_PATH = "bin\\XFoil\\xfoil.exe"
+#     AIRFOIL_PATH = "data\\turbines\\DTU_10MW\\Aero\\Airfoils\\FFA_W3_241.afl"
+
+#     xfoil = XFoil(XFOIL_PATH)
+#     top, bot = xfoil.run(AIRFOIL_PATH, 1E6, 0.2, 0)
+
+#     plt.plot(top["x/c"], top["y/c"])
+#     plt.plot(bot["x/c"], bot["y/c"])
+#     plt.show()
+
+#     plt.plot(top["x/c"], top["U_e/U"])
+#     plt.plot(bot["x/c"], bot["U_e/U"])
+#     plt.show()
+
+#     plt.plot(top["x/c"], top["delta_star"])
+#     plt.plot(bot["x/c"], bot["delta_star"])
+#     plt.show()
+
+#     plt.plot(top["x/c"], top["theta"])
+#     plt.plot(bot["x/c"], bot["theta"])
+#     plt.show()
+
+#     plt.plot(top["x/c"], top["C_f"])
+#     plt.plot(bot["x/c"], bot["C_f"])
+#     plt.show()
+
+#     plt.plot(top["x/c"], top["H"])
+#     plt.plot(bot["x/c"], bot["H"])
+#     plt.show()
