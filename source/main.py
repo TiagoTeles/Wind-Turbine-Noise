8c43c4d8b9619194cb1fa5af41d4a89796c6a207
Tiago Teles <teletiago1@hotmail.com>
2025-03-14 14:29:29 +0000

    Clean and organise file

--- source/main.py
+++ source/main.py
@@ -1,54 +1,73 @@
+"""
+Author:   T. Moreira da Fonte Fonseca Teles
+Email:    tmoreiradafont@tudelft.nl
+Date:     2025-03-14
+License:  GNU GPL 3.0
+
+Main script to run a QBlade simulation using the Python API.
+
+Classes:
+    None
+
+Functions:
+    None
+
+Exceptions:
+    None
+"""
+
 import os
 import sys
 import shutil
 
-sys.path.append(os.path.dirname(sys.path[0]))
-
-from qblade import QBlade
-from simulation import Simulation
+from QBlade.qblade import QBlade
+from QBlade.simulation import Simulation
 
 # QBlade configuration
-DLL_PATH = "bin\\QBlade\\QBlade.dll"    # DLL path
-CL_DEVICE = 1                           # OpenCL device
-GROUP_SIZE = 32                         # OpenCL work-group size
+QBLADE_PATH = "bin\\QBlade\\QBladeCE_2.0.8.5.dll"                   # DLL path
+QBLADE_CL_DEVICE = 1                                                # OpenCL device
+QBLADE_GROUP_SIZE = 32                                              # OpenCL work-group size
 
 # Simulation specification
-TURBINE_NAME = "DTU_10MW"                                   # Turbine name
-TURBINE_PATH = "data\\turbines\\DTU_10MW\\DTU_10MW_RWT.trb" # Turbine definition path
-SIMULATION_PATH = "data\\simulations\\DTU_10MW_RWT.sim"     # Simulation definition path
-WORKING_PATH = "tmp\\QBlade"                                # Working directory path
-N_TIMESTEP = 500                                            # Number of timesteps
+TURBINE_PATH = "data\\turbines\\DTU_10MW_v1.3\\DTU_10MW_RWT.trb"    # Turbine definition path
+SIMULATION_PATH = "data\\simulations\\DTU_10MW_RWT.sim"             # Simulation definition path
+WORKING_DIR = "temp"                                                # Working directory
+N_TIMESTEP = 500                                                    # Number of timesteps, [-]
 
-# Copy files to working directory
-shutil.copytree(os.path.dirname(TURBINE_PATH), os.path.join(WORKING_PATH, TURBINE_NAME))
-shutil.copyfile(SIMULATION_PATH, os.path.join(WORKING_PATH, TURBINE_NAME + ".sim"))
+# Copy files to the working directory
+turbine_dir = os.path.dirname(TURBINE_PATH)
+turbine_name = os.path.splitext(os.path.basename(TURBINE_PATH))[0]
 
-# Read files
-simulation = Simulation(os.path.join(WORKING_PATH, TURBINE_NAME + ".sim"))
+simulation_name = os.path.splitext(os.path.basename(SIMULATION_PATH))[0]
 
-simulation.turbine.write("BEMSPEEDUP", 100.0)
-simulation.turbine.write("STRUCTURALFILE", "")
-simulation.turbine.write("CONTROLLERFILE", "")
-simulation.turbine.write("PARAMETERFILE", "")
-simulation.turbine.write("CONTROLLERTYPE", 0)
+turbine_work_dir = os.path.join(WORKING_DIR, simulation_name, turbine_name)
+simulation_work_path = os.path.join(WORKING_DIR, simulation_name, simulation_name + ".sim")
+
+if not os.path.exists(WORKING_DIR):
+    shutil.copytree(turbine_dir, turbine_work_dir)
+    shutil.copyfile(SIMULATION_PATH, simulation_work_path)
 
 # Start QBlade
-if os.path.exists(DLL_PATH):
-    print(f"Using shared library file: {DLL_PATH}!")
+if os.path.exists(QBLADE_PATH):
+    print(f"Using shared library file: {QBLADE_PATH}!")
 else:
-    print(f"No shared library file was found in {DLL_PATH}!")
+    print(f"No shared library file was found in {QBLADE_PATH}!")
     sys.exit(1)
 
-# Create an object that contains the API
-qblade = QBlade(DLL_PATH)
+qblade = QBlade(QBLADE_PATH)
+qblade.createInstance(QBLADE_CL_DEVICE, QBLADE_GROUP_SIZE)
 
-# Create a QBlade instance from the library
-qblade.createInstance(CL_DEVICE, GROUP_SIZE)
+# Configure the simulation
+simulation = Simulation(simulation_work_path)
 
-# Load a simulation definition file
-qblade.loadSimDefinition(os.path.join(WORKING_PATH, TURBINE_NAME + ".sim").encode("utf-8"))
+simulation.turbine.write("BEMSPEEDUP", 100.0)   # Speedup convergence
+simulation.turbine.write("STRUCTURALFILE", "")  # Disable structural simulation
+simulation.turbine.write("CONTROLLERFILE", "")  # Disable controller simulation
+simulation.turbine.write("PARAMETERFILE", "")   # Disable controller simulation
+simulation.turbine.write("CONTROLLERTYPE", 0)   # Disable controller simulation
 
-# Initializing the simulation
+# Setup the simulation
+qblade.loadSimDefinition(simulation_work_path.encode("utf-8"))
 qblade.initializeSimulation()
 
 # Run the simulation
@@ -60,10 +79,10 @@ for i in range(N_TIMESTEP):
     # Ensure the simulation step was successful
     if not success:
         print(f"Simulation failed at timestep {i}!")
-        break
+        sys.exit(1)
 
-# Store the simulation results
-qblade.exportResults(0, WORKING_PATH.encode("utf-8"), "DTU_10MW_RWT".encode("utf-8"), b"")
+# Save the simulation results
+qblade.exportResults(0, os.path.join(WORKING_DIR, simulation_name).encode("utf-8"), simulation_name.encode("utf-8"), b"")
 
 # Unload the QBlade library
 qblade.unload_library()
