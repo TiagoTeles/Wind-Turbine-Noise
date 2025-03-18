77b64e70ae9af16826df6264e72ef95dd78eef0d
Tiago Teles <teletiago1@hotmail.com>
2025-03-18 16:12:04 +0000

    Format settings

--- source/settings.py
+++ source/settings.py
@@ -3,49 +3,38 @@ Author:   T. Moreira da Fonte Fonseca Teles
 Email:    tmoreiradafont@tudelft.nl
 Date:     2025-03-18
 License:  GNU GPL 3.0
-
-Settings for the various scripts.
-
-Classes:
-    None
-
-Functions:
-    None
-
-Exceptions:
-    None
 """
 
-# QBlade configuration
-QBLADE_PATH = "bin\\QBlade\\QBladeCE_2.0.8.5.dll"                   # QBlade.DLL path
-QBLADE_CL_DEVICE = 1                                                # OpenCL device
+# QBlade settings
+QBLADE_PATH = "bin\\QBlade\\QBladeCE_2.0.8.5.dll"                   # QBlade.dll path
+QBLADE_CL_DEVICE = 0                                                # OpenCL device
 QBLADE_GROUP_SIZE = 32                                              # OpenCL work-group size
 
-# XFOIL configuration
+# XFOIL settings
 XFOIL_PATH = "bin\\XFoil\\xfoil.exe"                                # XFOIL.exe path
+MAX_ITER = 100                                                      # Iteration limit, [-]
 
-# Simulation specification
-TURBINE_PATH = "data\\turbines\\DTU_10MW_v1.3\\DTU_10MW_RWT.trb"    # Turbine definition path
-SIMULATION_PATH = "data\\simulations\\DTU_10MW_RWT.sim"             # Simulation definition path
-WORKING_DIR = "temp"                                                # Working directory
+# Simulation settings
+TURBINE_PATH = "data\\turbines\\DTU_10MW_v1.3\\DTU_10MW_RWT.trb"    # Turbine path
+SIMULATION_PATH = "data\\simulations\\DTU_10MW_RWT.sim"             # Simulation path
 N_TIMESTEP = 500                                                    # Number of timesteps, [-]
-BLADE = 1                                                           # Blade ID TODO: Does not work for unsteady case!
-TIMESTEP = -1                                                       # Timestep index to use
+TIMESTEP = -1                                                       # Timestep index
+BLADE = 1                                                           # Blade index
 
-# Aerodynamic settings
-C_0 = 340.0                                                         # Speed of sound, [m/s] TODO: Base on temperature?
-RHO_0 = 1.225                                                       # Air density, [kg/m^3] TODO: Use QBalde value?
-Z_0 = 0.01                                                          # Surface roughness length, [m] TODO: Base on what?
+# Atmospheric settings
+C_0 = 340.0                                                         # Speed of sound, [m/s]
+RHO_0 = 1.225                                                       # Air density, [kg/m^3]
+Z_0 = 0.01                                                          # Surface roughness length, [m]
 
 # Acoustic settings
-BASE_10 = True                                                      # Use base 10?
+P_REF = 2E-5                                                        # Reference pressure, [Pa]
 F_MIN = 20                                                          # Minimum frequency, [Hz]
 F_MAX = 20000                                                       # Maximum frequency, [Hz]
 F_REF = 1000                                                        # Reference frequency, [Hz]
-P_REF = 2E-5                                                        # Reference pressure, [Pa]
+BASE_10 = True                                                      # Use base 10?
 
-# Aeroacosutic settings
-SPL_CORRECTION = True                                               # Apply inflow noise 10dB SPL correction?
+# Aeroacoustic settings
+RADIAL_CUTOFF = 0.4                                                 # Smallest radial position section, [-]
+SPL_CORRECTION = True                                               # Use inflow noise 10dB SPL correction?
 PROBE_TOP = 0.975                                                   # Probe location at the top surface, [-]
 PROBE_BOT = 0.950                                                   # Probe location at the bottom surface, [-]
-THICKNESS_CUTOFF = 0.3                                              # Thickness cutoff for the airfoil, [-]
