fa5d22cbaef8b08706c43bc7cdd14b36b34faff1
Tiago Teles <teletiago1@hotmail.com>
2025-03-26 12:54:54 +0000

    Format file

--- source/aeroacoustics/trailing_edge_noise.py
+++ source/aeroacoustics/trailing_edge_noise.py
@@ -11,13 +11,16 @@ Classes:
 
 # Functions:
     delta_star
-    TODO
+    wall_pressure
+    correlation_length
+    te_noise
 
 Exceptions:
     None
 """
 
 import os
+import sys
 
 import numpy as np
 
@@ -26,7 +29,7 @@ from settings import XFOIL_PATH
 from xfoil import XFoil
 
 
-def delta_star(blade, pos, Re, M, alpha, cutoff, probe_top, probe_bot, it):
+def displacement_thickness(blade, pos, Re, M, alpha, cutoff, probe_top, probe_bot, it):
     """
     Determine the boundary layer displacement thickness.
 
@@ -46,6 +49,12 @@ def delta_star(blade, pos, Re, M, alpha, cutoff, probe_top, probe_bot, it):
         delta_star_bot : np.array -- bottom boundary layer displacement thickness, [m]
     """
 
+    # Remove the unused axis
+    Re = np.squeeze(Re)
+    M = np.squeeze(M)
+    alpha = np.squeeze(alpha)
+
+    # Initialise the numpy arrays
     delta_star_top = np.zeros(Re.shape)
     delta_star_bot = np.zeros(Re.shape)
 
@@ -64,7 +73,7 @@ def delta_star(blade, pos, Re, M, alpha, cutoff, probe_top, probe_bot, it):
 
             # Run XFoil
             xfoil = XFoil(XFOIL_PATH, cwd)
-            top, bot = xfoil.run(path, Re[i], M[i], alpha[i], it=it)
+            top, bot = xfoil.run(path, Re[i], M[i], np.degrees(alpha[i]), it=it)
 
             # Determine the displacement thickness at the probe locations
             delta_star_top[i] = np.interp(probe_top, top["x/c"], top["delta_star"])
@@ -79,91 +88,180 @@ def delta_star(blade, pos, Re, M, alpha, cutoff, probe_top, probe_bot, it):
     return delta_star_top, delta_star_bot
 
 
-def TODO(blade, pos, Re, alpha, cutoff, probe_top, probe_bot, it, f, p_ref, x, y, z, b_c,  c_0, rho_0, alpha_c, U, b, c):
+def wall_pressure(K_1, U, delta_star, rho_0):
     """
-    TODO
+    Determine the wall pressure spectrum.
+
+    Arguments:
+        K_1 : float -- streamwise wavenumber, [1/m]
+        U : np.array -- velocity, [m/s]
+        delta_star : np.array -- boundary layer displacement thickness, [m]
+        rho_0 : float -- air density, [kg/m^3]
+
+    Returns:
+        Phi_pp : np.array -- wall pressure spectrum, [Pa^2/Hz]
     """
 
-    # Determine the average boundary layer displacement thicknesses
-    # TODO: Paper uses flat plate at aoa=0, so bl is the same on top and bot
-    delta_star_top, delta_star_bot = delta_star(blade, pos, Re, M, alpha, cutoff, probe_top, probe_bot, it)
-    d_star = (delta_star_top + delta_star_bot) / 2
+    omega_tilde = K_1 * delta_star
 
-    omega = 2 * np.pi * f
-    K_x = omega / U
+    F = (33.28 * omega_tilde) / (1 - 5.489 * omega_tilde + 36.74 * np.square(omega_tilde) \
+      + 0.1505 * np.pow(omega_tilde, 5))
+
+    Phi_pp = np.square(0.5 * rho_0 * np.square(U)) * (delta_star / U) * 2E-5 * F
+
+    return Phi_pp
+
+
+def correlation_length(omega, K_1, K_2, U_c, delta_star, b_c):
+    """
+    Determine the spanwise correlation length.
+
+    Arguments:
+        omega : np.array -- angular frequency, [rad/s]
+        K_1 : float -- streamwise wavenumber, [1/m]
+        K_2 : float -- spanwise wavenumber, [1/m]
+        U_c : np.array -- convection velocity, [m/s]
+        delta_star : np.array -- boundary layer displacement thickness, [m]
+        b_c : float -- correlation coefficient, [-]
+
+    Returns:
+        l_y : np.array -- spanwise correlation length, [m]
+    """
+
+    # Determine omega_tilde
+    omega_tilde = K_1 * delta_star
+
+    # Determine the correlation length
+    l_y = (omega / (b_c * U_c)) / (np.square(K_2) + np.square(omega) / np.square(b_c * U_c))
+
+    # Apply the correction proposed by Casalino et al. (2019)
+    l_y *= (1 - np.exp(-np.square(omega_tilde) / 0.09))
+
+    return l_y
+
+
+def te_noise(f, blade, pos, b, c, x, y, z, U, Re, alpha, alpha_c, b_c, p_ref, c_0, rho_0, cutoff, \
+             probe_top, probe_bot, it):
+    """
+    Determine the trailing-edge noise spectra.
+
+    Arguments:
+        f : np.array -- frequency, [Hz]
+        blade : Blade -- blade object
+        pos : np.array -- position along the blade, [-]
+        b : np.array -- span, [m]
+        c : np.array -- chord, [m]
+        x : np.array -- x-coordinate, [m]
+        y : np.array -- y-coordinate, [m]
+        z : np.array -- z-coordinate, [m]
+        U : np.array -- velocity, [m/s]
+        Re : np.array -- Reynolds number, [-]
+        alpha : np.array -- angle of attack, [rad]
+        alpha_c : np.array -- speed ratio, [-]
+        b_c : np.array -- correlation coefficient, [-]
+        p_ref : float -- reference pressure, [Pa]
+        c_0 : float -- speed of sound, [m/s]
+        rho_0 : float -- air density, [kg/m^3]
+        cutoff : float -- radial cutoff, [-]
+        probe_top : np.array -- probe location at the top surface, [-]
+        probe_bot : np.array -- probe location at the bottom surface, [-]
+        it : int -- number of iterations for XFoil, [-]
+    """
+
+    # Determine the Mach number
     M = U / c_0
+
+    # Determine the Prandtl-Glauert factor
     beta = np.sqrt(1 - np.square(M))
+
+    # Determine distance corrected for convection effects
     S_0 = np.sqrt(np.square(x) + np.square(beta) * (np.square(y) + np.square(z)))
-    K_2 = k * y / S_0
-    U_c = U / alpha_c
+
+    # Determine the angular frequency
+    omega = 2 * np.pi * f
+
+    # Determine the acoustic wavenumber
     k = omega / c_0
+
+    # Determine the convection velocity
+    U_c = U / alpha_c
+
+    # Determine the convective wave number
     K = omega / U
+
+    # # Determine the aerodynamic wavenumbers
+    K_1 = alpha_c * K
+    K_2 = k * y / S_0
+
+    # Determine the non-dimensional wavenumbers
     K_line = K * (c/2)
-    mu_line = K_line * M / np.square(beta)
-    K_1_line = omega  * (c/2) / U_c    # TODO: CHECK IF THIS IS CORRECT
+    K_1_line = K_1 * (c/2)
     K_2_line = K_2 * (c/2)
+
+    # Determine the frequency parameters
+    mu_line = K_line * M / np.square(beta)
     kappa_line = np.sqrt(np.square(mu_line) - np.square(K_2_line) / np.square(beta))
 
-    if np.any(np.square(mu_line) - np.square(K_2_line) / np.square(beta) < 0):
+    if np.any(K_2_line > K_line * M / beta):
         print("Sub-critical gust detected!")
-        print("I probably need to implement them :(")
+        print("I should probably implement them :(")
+        sys.exit(1)
 
-    # Deteremine the first-order scattering term
-    # Neglect the term as inidcated in the paper
+    # Determine the correction factor
     epsilon = np.pow(1 + 1 / (4 * mu_line), -0.5)
 
+    # Determine the average boundary layer displacement thicknesses
+    delta_star_top, delta_star_bot = displacement_thickness(blade, pos, Re, M, alpha, cutoff, \
+                                                            probe_top, probe_bot, it)
+    delta_star = (delta_star_top + delta_star_bot) / 2
+
+    # Determine the first-order scattering term (neglect the term np.exp(-2 * 1j * C))
     B = K_1_line + M * mu_line + kappa_line
     C = K_1_line - mu_line * (x / S_0 - M)
 
-    uncorrected_1 = (1 + 1j) * np.exp(-2 * 1j * C) * np.sqrt(B/(B-C)) * E(2 * (B - C)) - (1 + 1j) * E(2 * B) + 1
-    corrected_1 = np.zeros(uncorrected_1.shape, dtype=complex)
-    corrected_1.real = uncorrected_1.real
-    corrected_1.imag = uncorrected_1.imag * epsilon
-
-    f_1 = - np.exp(2 * 1j * C) / (1j * C) * corrected_1
+    f_1 = - np.exp(2 * 1j * C) / (1j * C) * ((1 + 1j) * np.exp(-2 * 1j * C) \
+        * np.sqrt(B / (B - C)) * E(2 * (B - C)) - (1 + 1j) * E(2 * B) + 1)
 
     # Determine the second-order back-scattering correction
-    uncorrected_2 = np.exp(4 * 1j * kappa_line) * (1 - (1 + 1j) * E(4 * kappa_line))
-
-    corrected_2 = np.zeros(uncorrected_2.shape, dtype=complex)
-    corrected_2.real = uncorrected_2.real
-    corrected_2.imag = uncorrected_2.imag * epsilon
+    f_2 = np.exp(4 * 1j * kappa_line) * (1 - (1 + 1j) * E(4 * kappa_line))
+    f_2.imag = f_2.imag * epsilon
 
     D = kappa_line - mu_line * x / S_0
 
-    G = (1 + epsilon) * np.exp(1j * (2 * kappa_line + D)) * np.sin(D - 2 * kappa_line) / (D - 2 * kappa_line) + (1 - epsilon) * np.exp(1j * (-2 * kappa_line + D)) * np.sin(D + 2*kappa_line) / (D + 2 * kappa_line)
-    G += (1 + epsilon) * (1 - 1j) / (2 * (D - 2 * kappa_line)) * np.exp(4 * 1j * kappa_line) * E(4 * kappa_line) - (1 - epsilon) * (1 + 1j) / (2 * (D + 2 * kappa_line)) * np.exp(-4 * 1j * kappa_line) * E(4 * kappa_line)
-    G += np.exp(2 * 1j * D) / 2 * np.sqrt(2 * kappa_line / D) * E(2 * D) * ((1 + 1j) * (1 - epsilon) / (D + 2 * kappa_line) - (1 - 1j) * (1 + epsilon) / (D - 2 * kappa_line))
+    G = (1 + epsilon) * np.exp(1j * (2 * kappa_line + D)) * np.sin(D - 2 * kappa_line) \
+      / (D - 2 * kappa_line) + (1 - epsilon) * np.exp(1j * (-2 * kappa_line + D)) \
+      * np.sin(D + 2 * kappa_line) / (D + 2 * kappa_line) + (1 + epsilon) * (1 - 1j) \
+      / (2 * (D - 2 * kappa_line)) * np.exp(4 * 1j * kappa_line) * E(4 * kappa_line) \
+      - (1 - epsilon) * (1 + 1j) / (2 * (D + 2 * kappa_line)) * np.exp(-4 * 1j * kappa_line) \
+      * E(4 * kappa_line) + np.exp(2 * 1j * D) / 2 * np.sqrt(2 * kappa_line / D) * E(2 * D) \
+      * ((1 + 1j) * (1 - epsilon) / (D + 2 * kappa_line) - (1 - 1j) * (1 + epsilon) \
+      / (D - 2 * kappa_line))
 
-    f_2 = corrected_2 - np.exp(2 * 1j * D) + 1j * (D + K_line + M * mu_line - kappa_line) * G
+    f_2 = f_2 - np.exp(2 * 1j * D) + 1j * (D + K_line + M * mu_line - kappa_line) * G
 
     # Determine the radiation integral
-    Theta = np.sqrt((K_1_line + M * mu_line + kappa_line) / (K_line + M * mu_line + kappa_line))
-    H = (1 + 1j) * np.exp(-4 * 1j * kappa_line) * (1 - np.square(Theta)) / (2 * np.sqrt(np.pi) * (alpha_c - 1) * K_line * np.sqrt(B))
+    Theta = np.sqrt((K_1_line + mu_line * M + kappa_line) / (K_line + mu_line * M + kappa_line))
+
+    H = (1 + 1j) * np.exp(-4 * 1j * kappa_line) * (1 - np.square(Theta)) \
+      / (2 * np.sqrt(np.pi) * (alpha_c - 1) * K_line * np.sqrt(B))
+
     I = f_1 + H * f_2
 
-    # Determine the wall pressure spectrum using an empirical model from data from an airfoil
-    # TODO: Is there a newer model for F or Phi_pp
-    omega_tilde = K_x * d_star
-    F = (33.28 * omega_tilde) / (1 - 5.489 * omega_tilde + 36.74 * np.square(omega_tilde) + 0.1505 * np.pow(omega_tilde, 5))
-    Phi_pp = np.square(0.5 * rho_0 * np.square(U)) * (d_star / U) * 2E-5 * F
+    # Determine the wall pressure spectrum using Schlinker's model
+    Phi_pp = wall_pressure(K_1, U, delta_star, rho_0)
 
     # Determine the spanwise correlation length using Corcos' model
-    # TODO: Is this model good enough?
-    l_y = (omega / (b_c * U_c)) / (np.square(K_2) + np.square(omega) / np.square(b_c * U_c))
-    l_y *= (1 - np.exp(-np.square(omega_tilde) / 0.09)) # Prof. Dr. D. Casalino correction term
+    l_y = correlation_length(omega, K_1, K_2, U_c, delta_star, b_c)
 
     # Determine the streamwise-integrated wavenumber spectral density of wall-pressure ﬂuctuations
     Pi_0 = (1/np.pi) * Phi_pp * l_y
 
-    # Determine the double-sided farfield acoustic PSD. This equation simplifies if  b >> c, AKA if
-    # we look only at the priveleged oblique gust. This is investigated in the validation of Part 2
-    # TODO: Does the factor of 2 before np.pi model both airfoil surfaces? Comparing equations seems
-    # to indicate that it does. Also stated in Roger and Moreau (2005)
+    # Determine the double-sided farfield acoustic PSD. Only valid if  b >> c, AKA if we look only
+    # at the priveleged oblique gust. This is investigated in (Roger and Moreau, 2009).
     S_pp = np.square(omega * z * c / (4 * np.pi * c_0 * np.square(S_0))) * 2 * np.pi * b \
          * np.square(np.abs(I)) * Pi_0
 
     # Determine the 1/3 octave band SPL
-    SPL = 10 * np.log10((2*np.pi*(0.232*f)) * (2*S_pp) / np.square(p_ref))
+    SPL = 10 * np.log10(4 * np.pi * 0.232 * f * S_pp / np.square(p_ref))
 
     return SPL
