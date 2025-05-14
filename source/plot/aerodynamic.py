cb63b7bdc7d393fe73097d182f01049393a9fd8a
Tiago Teles <teletiago1@hotmail.com>
2025-05-14 17:42:06 +0100

    Add future files

---
+++ source/plot/aerodynamic.py
@@ -0,0 +1,17 @@
+""" 
+Author:   T. Moreira da Fonte Fonseca Teles
+Email:    tmoreiradafont@tudelft.nl
+Date:     2025-05-12
+License:  GNU GPL 3.0
+
+Plot the results of the aerodynamic simulation.
+
+Classes:
+    None
+
+Functions:
+    None
+
+Exceptions:
+    None
+"""
