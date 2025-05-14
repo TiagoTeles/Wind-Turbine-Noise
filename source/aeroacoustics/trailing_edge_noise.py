fa5e3bbc44925ed0b1802a12d329f124245ba6a1
Tiago Teles <teletiago1@hotmail.com>
2025-05-14 14:03:27 +0100

    Add fix

--- source/aeroacoustics/trailing_edge_noise.py
+++ source/aeroacoustics/trailing_edge_noise.py
@@ -199,6 +199,10 @@ def te_noise(f, b, c, x, y, z, U, delta_star, c_0, rho_0, p_ref, alpha_c, b_c, b
     S_pp = np.square(omega * z * c / (4 * np.pi * c_0 * np.square(S_0))) * 2 * np.pi * b \
          * np.square(np.abs(I)) * Pi_0
 
+    # Consider only one side of the airfoil
+    # TODO: Check if this is correct
+    # S_pp /= 4
+
     # Determine the bandwidth
     if base_10:
         f_lower = f / np.pow(10, 1/20)
