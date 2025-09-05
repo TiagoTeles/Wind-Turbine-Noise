b1540e383a5f457e29b4d7cd0e8bc40ae850a6b6
Tiago Teles <teletiago1@hotmail.com>
2025-09-05 14:15:30 +0100

    Move file

---
+++ source/acoustics/trailing_edge_noise.py
@@ -0,0 +1,17 @@
+"""
+Author:   T. Moreira da Fonte Fonseca Teles
+Email:    tmoreiradafont@tudelft.nl
+Date:     2025-09-05
+License:  GNU GPL 3.0
+
+Determine the TBLTE noise spectra.
+
+Classes:
+    None
+
+Functions:
+    TODO
+
+Exceptions:
+    None
+"""
