c66832dbcf0fdb0f99b0c7fbb434a58f0c33898e
Tiago Teles <teletiago1@hotmail.com>
2025-09-08 17:36:41 +0100

    Update coordinate transformation

--- source/coordinates.py
+++ source/coordinates.py
@@ -1,10 +1,10 @@
 """
 Author:   T. Moreira da Fonte Fonseca Teles
 Email:    tmoreiradafont@tudelft.nl
-Date:     2025-05-12
+Date:     2025-09-08
 License:  GNU GPL 3.0
 
-Coordinate system transformations.
+Determine the coordinate system transformations.
 
 Classes:
     None
@@ -27,162 +27,162 @@ import sys
 import numpy as np
 
 
-def transform(p_x, p_y, p_z, r_x, r_y, r_z, order):
+def transform(t_x, t_y, t_z, r_x, r_y, r_z, order):
     """
-    Returns the transformation matrix.
+    Determine the transformation matrix.
 
     Parameters:
-        p_x : float -- translation in the x direction, [m]
-        p_y : float -- translation in the y direction, [m]
-        p_z : float -- translation in the z direction, [m]
-        r_x : float -- rotation about the x axis, [rad]
-        r_y : float -- rotation about the y axis, [rad]
-        r_z : float -- rotation about the z axis, [rad]
-        order : str -- order of the rotation
+        t_x : float -- translation in the x direction, [m]
+        t_y : float -- translation in the y direction, [m]
+        t_z : float -- translation in the z direction, [m]
+        r_x : float -- rotation about the x-axis, [rad]
+        r_y : float -- rotation about the y-axis, [rad]
+        r_z : float -- rotation about the z-axis, [rad]
+        order : str -- rotation order
     """
 
-    # Determine the x rotation matrix
-    m_rot_x = np.array([[1,           0,            0, 0],
-                        [0, np.cos(r_x), -np.sin(r_x), 0],
-                        [0, np.sin(r_x),  np.cos(r_x), 0],
-                        [0,            0,           0, 1]])
+    # Determine the rotation matrix about the x-axis
+    rotation_x = np.array([[1.0,         0.0,          0.0, 0.0],
+                           [0.0, np.cos(r_x), -np.sin(r_x), 0.0],
+                           [0.0, np.sin(r_x),  np.cos(r_x), 0.0],
+                           [0.0,         0.0,          0.0, 1.0]])
 
-    # Determine the y rotation matrix
-    m_rot_y = np.array([[ np.cos(r_y), 0, np.sin(r_y), 0],
-                        [           0, 1,           0, 0],
-                        [-np.sin(r_y), 0, np.cos(r_y), 0],
-                        [           0, 0,           0, 1]])
+    # Determine the rotation matrix about the y-axis
+    rotation_y = np.array([[ np.cos(r_y), 0.0, np.sin(r_y), 0.0],
+                           [         0.0, 1.0,         0.0, 0.0],
+                           [-np.sin(r_y), 0.0, np.cos(r_y), 0.0],
+                           [         0.0, 0.0,         0.0, 1.0]])
 
-    # Determine the z rotation matrix
-    m_rot_z = np.array([[np.cos(r_z), -np.sin(r_z), 0, 0],
-                        [np.sin(r_z),  np.cos(r_z), 0, 0],
-                        [          0,            0, 1, 0],
-                        [          0,            0, 0, 1]])
+    # Determine the rotation matrix about the z axis
+    rotation_z = np.array([[np.cos(r_z), -np.sin(r_z), 0.0, 0.0],
+                           [np.sin(r_z),  np.cos(r_z), 0.0, 0.0],
+                           [        0.0,          0.0, 1.0, 0.0],
+                           [        0.0,          0.0, 0.0, 1.0]])
 
     # Determine the translation matrix
-    m_trans = np.array([[1, 0, 0, p_x],
-                        [0, 1, 0, p_y],
-                        [0, 0, 1, p_z],
-                        [0, 0, 0,   1]])
+    translation = np.array([[1.0, 0.0, 0.0, t_x],
+                            [0.0, 1.0, 0.0, t_y],
+                            [0.0, 0.0, 1.0, t_z],
+                            [0.0, 0.0, 0.0, 1.0]])
 
     # Determine the transformation matrix
     if order == "xyz":
-        return m_trans @ m_rot_z @ m_rot_y @ m_rot_x
+        return translation @ rotation_z @ rotation_y @ rotation_x
 
     elif order == "xzy":
-        return m_trans @ m_rot_y @ m_rot_z @ m_rot_x
+        return translation @ rotation_y @ rotation_z @ rotation_x
 
     elif order == "yxz":
-        return m_trans @ m_rot_z @ m_rot_x @ m_rot_y
+        return translation @ rotation_z @ rotation_x @ rotation_y
 
     elif order == "yzx":
-        return m_trans @ m_rot_x @ m_rot_z @ m_rot_y
+        return translation @ rotation_x @ rotation_z @ rotation_y
 
     elif order == "zxy":
-        return m_trans @ m_rot_y @ m_rot_x @ m_rot_z
+        return translation @ rotation_y @ rotation_x @ rotation_z
 
     elif order == "zyx":
-        return m_trans @ m_rot_x @ m_rot_y @ m_rot_z
+        return translation @ rotation_x @ rotation_y @ rotation_z
 
     else:
         print("Invalid order of rotation!")
         sys.exit(1)
 
 
-def turbine_to_global(p_x, p_y, p_z, r_x, r_y, r_z):
+def turbine_to_global(t_x, t_y, t_z, r_x, r_y, r_z):
     """
-    Returns the transformation matrix from the turbine 
+    Determine the transformation matrix from the turbine
     coordinate system to the global coordinate system.
 
     Parameters:
-        p_x : float -- translation in the x direction, [m]
-        p_y : float -- translation in the y direction, [m]
-        p_z : float -- translation in the z direction, [m]
-        r_x : float -- rotation about the x axis, [rad]
-        r_y : float -- rotation about the y axis, [rad]
-        r_z : float -- rotation about the z axis, [rad]
+        t_x : float -- translation in the x direction, [m]
+        t_y : float -- translation in the y direction, [m]
+        t_z : float -- translation in the z direction, [m]
+        r_x : float -- rotation about the x-axis, [rad]
+        r_y : float -- rotation about the y-axis, [rad]
+        r_z : float -- rotation about the z-axis, [rad]
 
     Returns:
         matrix : np.array -- transformation matrix
     """
 
-    return transform(p_x, p_y, p_z, r_x, r_y, r_z, "zyx")
+    return transform(t_x, t_y, t_z, r_x, r_y, r_z, "zyx")
 
 
-def nacelle_to_turbine(tower_height, shaft_tilt, yaw):
+def nacelle_to_turbine(tower_height, shaft_tilt_angle, phi):
     """
-    Returns the transformation matrix from the nacelle 
+    Determine the transformation matrix from the nacelle
     coordinate system to the turbine coordinate system.
 
     Parameters:
         tower_height : float -- tower height, [m]
-        shaft_tilt : float -- shaft tilt angle, [rad]
-        yaw : float -- yaw angle, [rad]
+        shaft_tilt_angle : float -- shaft tilt angle, [rad]
+        phi : float -- yaw angle, [rad]
 
     Returns:
         matrix : np.array -- transformation matrix
     """
 
-    return transform(0, 0, tower_height, 0, shaft_tilt, yaw, "xyz")
+    return transform(0.0, 0.0, tower_height, 0.0, shaft_tilt_angle, phi, "xyz")
 
 
-def hub_to_nacelle(overhang, azimuth):
+def hub_to_nacelle(rotor_overhang, psi):
     """
-    Returns the transformation matrix from the hub 
+    Determine the transformation matrix from the hub
     coordinate system to the nacelle coordinate system.
 
     Parameters:
-        overhang : float -- rotor overhang, [m]
-        azimuth : float -- azimuthal rotor angle, [rad]
+        rotor_overhang : float -- rotor overhang, [m]
+        psi : float -- azimuth angle, [rad]
 
     Returns:
         matrix : np.array -- transformation matrix
     """
 
-    return transform(-overhang, 0, 0, azimuth, 0, 0, "xyz")
+    return transform(-rotor_overhang, 0.0, 0.0, psi, 0.0, 0.0, "xyz")
 
 
-def blade_to_hub(cone, pitch):
+def blade_to_hub(rotor_cone_angle, theta):
     """
-    Returns the transformation matrix from the blade 
+    Determine the transformation matrix from the blade
     coordinate system to the hub coordinate system.
 
     Parameters:
-        cone : float -- rotor cone angle, [rad]
-        pitch : float -- collective blade pitch, [rad]
+        rotor_cone_angle : float -- rotor cone angle, [rad]
+        theta : float -- blade pitch angle, [rad]
 
     Returns:
         matrix : np.array -- transformation matrix
     """
 
-    return transform(0, 0, 0, 0, -cone, -pitch, "xzy")
+    return transform(0.0, 0.0, 0.0, 0.0, -rotor_cone_angle, -theta, "xzy")
 
 
-def airfoil_to_blade(pos, chord, twist, offset_x, offset_y, p_axis, x_c):
+def airfoil_to_blade(radius, chord, twist_angle, offset_x, offset_y, pitch_axis, x_c):
     """
-    Returns the transformation matrix from the airfoil 
+    Determine the transformation matrix from the airfoil
     coordinate system to the blade coordinate system.
 
     Parameters:
-        pos : float -- radius of the airfoil, [m]
-        chord : float -- chord of the airfoil, [m]
-        twist : float -- twist angle of the airfoil, [rad]
-        offset_x : float -- offset of the airfoil in the x-direction, [m]
-        offset_y : float -- offset of the airfoil in the y-direction, [m]
-        p_axis : float -- pitch axis, [-]
-        x_c : float -- coordinate system orgin, [-]
+        radius : float -- spanwise position, [m]
+        chord : float -- chord, [m]
+        twist_angle : float -- twist angle, [rad]
+        offset_x : float -- offset in the x-direction, [m]
+        offset_y : float -- offset in the y-direction, [m]
+        pitch_axis : float -- pitch axis position, [-]
+        x_c : float -- origin position, [-]
 
     Returns:
         matrix : np.array -- transformation matrix
     """
 
-    return transform(offset_y, offset_x, pos, np.pi/2, 0, np.pi/2 - twist, "xyz") \
-           @ transform((x_c-p_axis) * chord, 0, 0, 0, 0, 0, "xyz")
+    return transform(offset_y, offset_x, radius, np.pi/2.0, 0.0, np.pi/2.0 - twist_angle, "xyz") \
+           @ transform((x_c - pitch_axis) * chord, 0.0, 0.0, 0.0, 0.0, 0.0, "xyz")
 
 
 def freestream_to_airfoil(alpha):
     """
-    Returns the transformation matrix from the freestream 
+    Determine the transformation matrix from the freestream
     coordinate system to the airfoil coordinate system.
 
     Parameters:
@@ -192,4 +192,4 @@ def freestream_to_airfoil(alpha):
         matrix : np.array -- transformation matrix
     """
 
-    return transform(0, 0, 0, 0, -alpha, 0, "xyz")
+    return transform(0.0, 0.0, 0.0, 0.0, -alpha, 0.0, "xyz")
