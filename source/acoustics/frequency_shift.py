4b5231984b992114ab3b26c9551e806d9a36c9b4
Tiago Teles <teletiago1@hotmail.com>
2025-09-15 14:33:31 +0100

    Update function

--- source/acoustics/frequency_shift.py
+++ source/acoustics/frequency_shift.py
@@ -19,31 +19,30 @@ Exceptions:
 import numpy as np
 
 
-def frequency_shift(f_s, r, v_s, c_0):
+def frequency_shift(f_s, x_s, x_o, v_s, v_o, c_0):
     """
     Determine the frequency shift due to the Doppler effect.
 
     Parameters:
         f_s : np.array -- source frequency, [Hz]
-        r : np.array -- source-observer vector, [m]
-        v_s : np.array -- source velocity vector, [m/s]
+        x_s : np.array -- source position, [m]
+        x_o : np.array -- observer position, [m]
+        v_s : np.array -- source velocity, [m/s]
+        v_o : np.array -- observer velocity, [m/s]
         c_0 : float -- speed of sound, [m/s]
 
     Returns:
         f_o : np.array -- observer frequency, [Hz]
     """
 
-    # Determine the dot product of r and v_s
-    dot_product = r[0, :] * v_s[0, :] + r[1, :] * v_s[1, :] + r[2, :] * v_s[2, :]
+    # Determine the source-observer unit vector
+    r_so = (x_o - x_s) / np.linalg.norm(x_o - x_s, axis=0)
 
-    # Determine the norm of r and v_s
-    r_norm = np.linalg.norm(r, axis=0)
-    v_s_norm = np.linalg.norm(v_s, axis=0)
-
-    # Determine the angle between r and v_s
-    theta = np.arccos(dot_product / (r_norm * v_s_norm))
+    # Determine the velocity in the direction of r_so
+    v_s_proj = np.sum(v_s * r_so, axis=0)
+    v_o_proj = np.sum(v_o * r_so, axis=0)
 
     # Determine the observer frequency
-    f_o = f_s * c_0 / (c_0 - v_s_norm * np.cos(theta))
+    f_o = f_s * (c_0 - v_o_proj) / (c_0 - v_s_proj)
 
     return f_o
