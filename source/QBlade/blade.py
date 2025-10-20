4bbd3cad8d8158b45dc1a4cef286c3a7045e879e
Tiago Teles <teletiago1@hotmail.com>
2025-10-20 17:19:42 +0100

    Add function to read results

--- source/QBlade/blade.py
+++ source/QBlade/blade.py
@@ -1,7 +1,7 @@
 """
 Author:   T. Moreira da Fonte Fonseca Teles
 Email:    tmoreiradafont@tudelft.nl
-Date:     2025-09-22
+Date:     2025-10-20
 License:  GNU GPL 3.0
 
 Store the blade data.
@@ -17,13 +17,15 @@ Exceptions:
 """
 
 import os
+import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy as sp
 
 from source.QBlade.polar import Polar
-from source.settings import SIMULATION_PATH, AR
+from source.settings import QBLADE_SIMULATION_PATH, AR
 
 
 class Blade():
@@ -32,7 +34,9 @@ class Blade():
 
     Methods:
         __init__ -- initialise the Blade class
-        get -- get the blade property at a given radius
+        get -- get the blade geometry
+        read_results -- read the simulation results
+        get_results -- get the simulation results
         discretise -- discretise the blade into panels
 
     Attributes:
@@ -46,6 +50,16 @@ class Blade():
         polar : np.ndarray -- polar data
         thickness_01 : np.ndarray -- airfoil thickness at x/c = 0.01 [-]
         thickness_10 : np.ndarray -- airfoil thickness at x/c = 0.10 [-]
+        pitch : np.ndarray -- pitch angle, [rad]
+        azimuth : np.ndarray -- azimuthal angle, [rad]
+        angle_of_attack : np.ndarray -- angle of attack, [rad]
+        panel_radius : np.ndarray -- panel radius, [m]
+        axial_force : np.ndarray -- axial force per unit span, [N/m]
+        tangential_force : np.ndarray -- tangential force per unit span, [N/m]
+        velocity : np.ndarray -- velocity, [m/s]
+        beam_radius : np.ndarray -- beam radius, [m]
+        axial_deflection : np.ndarray -- axial deflection, [m]
+        radial_twist : np.ndarray -- radial twist, [rad]
     """
 
     def __init__(self, path):
@@ -91,9 +105,21 @@ class Blade():
             self.thickness_01[i] = self.polar[i].airfoil.thickness(0.01)
             self.thickness_10[i] = self.polar[i].airfoil.thickness(0.10)
 
+        # Initialise the results attributes
+        self.pitch = None
+        self.azimuth = None
+        self.angle_of_attack = None
+        self.panel_radius = None
+        self.axial_force = None
+        self.tangential_force = None
+        self.velocity = None
+        self.beam_radius = None
+        self.axial_deflection = None
+        self.radial_twist = None
+
     def get(self, key, radius):
         """
-        Get the blade geometry at a given radius.
+        Get the blade geometry.
 
         Parameters:
             key : str -- property key
@@ -105,6 +131,114 @@ class Blade():
 
         # Determine the value
         value = np.interp(radius, self.radius, getattr(self, key))
+
+        return value
+
+    def read_results(self, results):
+        """
+        Read the simulation results.
+
+        Parameters:
+            results : pd.DataFrame -- simulation results
+
+        Returns:
+            None
+        """
+
+        # Determine the number of timesteps
+        n_timesteps = len(results["Time~[s]"])
+
+        # Determine the number of panels and beams
+        n_panels = 0
+        n_beams = 0
+
+        for column in results.columns.values:
+            if "Radius~BLD_1~PAN_" in column:
+                n_panels += 1
+
+            if "X_l~For.~BLD_1~pos~" in column:
+                n_beams += 1
+
+        # Read the panel and beam radiuses
+        self.panel_radius = np.empty(n_panels)
+        self.beam_radius = np.linspace(self.radius[0], self.radius[-1], n_beams, endpoint=True)
+
+        for i in range(n_panels):
+            self.panel_radius[i] = results[f"Radius~BLD_1~PAN_{i}~[m]"].iloc[0]
+
+        # Read the Blade results
+        self.pitch = results["Pitch~Angle~BLD_1~[deg]"].to_numpy()
+        self.azimuth = results["Azimuthal~Angle~BLD_1~[deg]"].to_numpy()
+
+        # Read the aerodynamic blade distributions
+        self.angle_of_attack = np.empty((n_timesteps, n_panels))
+        self.axial_force = np.empty((n_timesteps, n_panels))
+        self.tangential_force = np.empty((n_timesteps, n_panels))
+        self.velocity = np.empty((n_timesteps, n_panels))
+
+        for i in range(n_panels):
+            self.angle_of_attack[:, i] = results[f"Angle~of~Attack~at~0.25c~BLD_1~PAN_{i}~[deg]"]
+            self.axial_force[:, i] = results[f"Normal~Force~BLD_1~PAN_{i}~[N/m]"]
+            self.tangential_force[:, i] = results[f"Tangential~Force~BLD_1~PAN_{i}~[N/m]"]
+            self.velocity[:, i] = results[f"Total~Velocity~BLD_1~PAN_{i}~[m/s]"]
+
+        # Read the structural blade distributions
+        self.axial_deflection = np.empty((n_timesteps, n_beams))
+        self.radial_twist = np.empty((n_timesteps, n_beams))
+
+        for i in range(n_beams):
+            self.axial_deflection[:, i] = results[f"X_b~Trl.Def.~BLD_1~pos~{(i / (n_beams - 1)):.3f}~[m]"]
+            self.radial_twist[:, i] = results[f"Z_b~Rot.Def.~BLD_1~pos~{(i / (n_beams - 1)):.3f}~[deg]"]
+
+        # Convert the angles from [deg] to [rad]
+        self.pitch = np.radians(self.pitch)
+        self.azimuth = np.radians(self.azimuth)
+        self.angle_of_attack = np.radians(self.angle_of_attack)
+        self.radial_twist = np.radians(self.radial_twist)
+
+    def get_results(self, key, azimuth, radius):
+        """
+        Get the simulation results.
+
+        Parameters:
+            key : str -- member key
+            azimuth : float -- azimuth angle, [deg]
+            radius : np.ndarray -- radius, [m]
+        """
+
+        # Check if the key is valid
+        if key in ["pitch", "azimuth"]:
+
+            # Interpolate the results
+            value = np.interp(azimuth, self.azimuth, getattr(self, key))
+
+        elif key in ["angle_of_attack", "axial_force", "tangential_force", "velocity"]:
+
+            # Create a 2D interpolator
+            interpolator = sp.interpolate.RegularGridInterpolator((self.azimuth, self.panel_radius), \
+                           getattr(self, key), bounds_error=False, fill_value=None)
+            
+            # Determine the meshgrid
+            azimuth, radius = np.meshgrid(azimuth, radius, indexing="ij")
+
+            # Interpolate the results
+            value = interpolator((azimuth, radius))
+
+        elif key in ["axial_deflection", "radial_twist"]:
+
+            # Create a 2D interpolator
+            interpolator = sp.interpolate.RegularGridInterpolator((self.azimuth, self.beam_radius), \
+                           getattr(self, key), bounds_error=False, fill_value=None)
+            
+            # Determine the meshgrid
+            azimuth, radius = np.meshgrid(azimuth, radius, indexing="ij")
+
+            # Interpolate the results
+            value = interpolator((azimuth, radius))
+
+        else:
+            print("Invalid key!")
+            sys.exit(1)
 
         return value
 
@@ -174,7 +308,7 @@ if __name__ == "__main__":
     from source.QBlade.simulation import Simulation
 
     # Create the Blade object
-    simulation = Simulation(SIMULATION_PATH)
+    simulation = Simulation(QBLADE_SIMULATION_PATH)
     turbine = simulation.turbine
     blade = turbine.blade
 
