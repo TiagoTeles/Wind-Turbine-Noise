ce190f62d8daffda034f98ba64101a0089d2ab0e
Tiago Teles <teletiago1@hotmail.com>
2025-10-21 12:27:42 +0100

    Change / to \\

--- verification/settings.py
+++ verification/settings.py
@@ -5,6 +5,6 @@ Date:     2025-10-20
 License:  GNU GPL 3.0
 """
 
-# Aerodynamic verification settings
-HAWC2_PATH = "verification/data/aerodynamic/HAWC2.csv"          # HAWC2 verification data path
-OPENFAST_PATH = "verification/data/aerodynamic/OpenFAST.csv"    # OpenFAST verification data path
+# Aerodynamic verification
+HAWC2_PATH = "verification\\data\\aerodynamic\\HAWC2.csv"       # HAWC2 data path
+OPENFAST_PATH = "verification\\data\\aerodynamic\\OpenFAST.csv" # OpenFAST data path
