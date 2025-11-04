24faba3f83d54f8e9d0ab72c925a9bc62694ef38
Tiago Teles <teletiago1@hotmail.com>
2025-11-04 12:28:08 +0000

    Move files

--- verification/source/settings.py
+++ verification/source/settings.py
@@ -1,10 +1,10 @@
 """
 Author:   T. Moreira da Fonte Fonseca Teles
 Email:    tmoreiradafont@tudelft.nl
-Date:     2025-10-20
+Date:     2025-11-04
 License:  GNU GPL 3.0
 """
 
-# Aerodynamic verification
-HAWC2_PATH = "verification\\data\\aerodynamic\\HAWC2.csv"       # HAWC2 data path
-OPENFAST_PATH = "verification\\data\\aerodynamic\\OpenFAST.csv" # OpenFAST data path
+# QBlade verification
+HAWC2_PATH = "verification\\data\\aerodynamics\\HAWC2.csv"       # HAWC2 verification data path
+OPENFAST_PATH = "verification\\data\\aerodynamics\\OpenFAST.csv" # OpenFAST verification data path
