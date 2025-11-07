d775e5f8783d3504a7fc4e12a29c96850a03a0be
Tiago Teles <teletiago1@hotmail.com>
2025-11-07 14:39:08 +0000

    Add verification data paths

--- verification/source/settings.py
+++ verification/source/settings.py
@@ -6,5 +6,13 @@ License:  GNU GPL 3.0
 """
 
 # QBlade verification
-HAWC2_PATH = "verification\\data\\aerodynamics\\HAWC2.csv"       # HAWC2 verification data path
-OPENFAST_PATH = "verification\\data\\aerodynamics\\OpenFAST.csv" # OpenFAST verification data path
+HAWC2_PATH = "verification\\data\\aerodynamics\\HAWC2.csv"          # HAWC2 verification data path
+OPENFAST_PATH = "verification\\data\\aerodynamics\\OpenFAST.csv"    # OpenFAST verification data path
+
+# Aeroacoustic vertification
+TDS_PATH = "verification\\data\\aeroacoustics\\3DS.csv"     # 3DS verification data path
+DLR_PATH = "verification\\data\\aeroacoustics\\DLR.csv"     # DLR verification data path
+DTU_PATH = "verification\\data\\aeroacoustics\\DTU.csv"     # DTU verification data path
+IAG_PATH = "verification\\data\\aeroacoustics\\IAG.csv"     # IAG verification data path
+NREL_PATH = "verification\\data\\aeroacoustics\\NREL.csv"   # NREL verification data path
+TNO_PATH = "verification\\data\\aeroacoustics\\TNO.csv"     # TNO verification data path
