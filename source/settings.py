d804d2063fa52338089a40cfdb2c059a0016c092
Tiago Teles <teletiago1@hotmail.com>
2025-11-10 11:36:42 +0000

    Minor changes

--- source/settings.py
+++ source/settings.py
@@ -1,7 +1,7 @@
 """
 Author:   T. Moreira da Fonte Fonseca Teles
 Email:    tmoreiradafont@tudelft.nl
-Date:     2025-10-21
+Date:     2025-11-10
 License:  GNU GPL 3.0
 """
 
@@ -13,7 +13,7 @@ P_0 = 101325.0  # Pressure, [Pa]
 T_0 = 288.15    # Temperature, [K]
 H_R = 0.80      # Relative humidity, [-]
 
-# Study domain properties
+# Wind turbine properties
 LAT = np.radians(41.6865)   # Latitude, [rad]
 LON = np.radians(-9.0574)   # Longitude, [rad]
 
@@ -32,22 +32,31 @@ QBLADE_OPENCL_GROUP_SIZE = 32                           # OpenCL work-group size
 QBLADE_SIMULATION_PATH = "data\\wind_turbine\\IEA_22MW_RWT\\IEA-22-280-RWT-Monopile.sim"    # Simulation path
 QBLADE_RESULTS_PATH = "results\\wind_turbine\\IEA_22MW_RWT\\qblade.txt"                     # Results path
 
-# Acoustic settings
+# One-Third octave settings
+BASE_10 = True      # Use base-10 formulation?
 F_MIN = 20.0        # Minimum frequency, [Hz]
 F_MAX = 20000.0     # Maximum frequency, [Hz]
 
-# Aeroacoustic settings
+# Aeroacoustic discretisation settings
 N_AZIMUTH = 12      # Number of azimuthal positions, [-]
 ASPECT_RATIO = 2.0  # Panel aspect ratio, [-]
-THRESHOLD = 0.4     # Radial cutoff, [-]
+RADIUS_CUTOFF = 0.4 # Radial cutoff, [-]
 
 # XFOIL settings
 XFOIL_PATH = "bin\\XFOIL\\xfoil.exe"    # xfoil.exe path
 XFOIL_MAX_ITERATIONS = 100              # Iteration limit, [-]
-XFOIL_UPPER_TRANSITION = 0.065          # Upper transition location, [-]
-XFOIL_LOWER_TRANSITION = 0.200          # Lower transition location, [-]
+XFOIL_TRANSITION_UPPER = 0.065          # Upper transition position, [-]
+XFOIL_TRANSITION_LOWER = 0.200          # Lower transition position, [-]
 XFOIL_AMPLIFICATION = 9.0               # Critical amplification factor, [-]
 
-# TE noise settings
-PROBE_UPPER = 0.975 # Suction side probe location, [-]
-PROBE_LOWER = 0.950 # Pressure side probe location, [-]
+# Probe settings
+PROBE_UPPER = 0.975 # Upper probe position, [-]
+PROBE_LOWER = 0.950 # Lower probe position, [-]
+
+# # NM80 settings
+# # P_0 = 101325.0  # Pressure, [Pa]
+# # T_0 = 292.15    # Temperature, [K]
+# # H_R = 0.70      # Relative humidity, [-]
+
+# # QBLADE_SIMULATION_PATH = "data\\wind_turbine\\NM_2MW_RWT\\NM-2-80-RWT.sim"                  # Simulation path
+# # QBLADE_RESULTS_PATH = "results\\wind_turbine\\NM_2MW_RWT\\qblade.txt"                       # Results path
