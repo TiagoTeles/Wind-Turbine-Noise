133fc2e2615ac70f6ba768b1374767191ef2d179
Tiago Teles <teletiago1@hotmail.com>
2025-11-11 14:57:40 +0000

    Minor changes

--- verification/source/settings.py
+++ verification/source/settings.py
@@ -1,18 +1,18 @@
 """
 Author:   T. Moreira da Fonte Fonseca Teles
 Email:    tmoreiradafont@tudelft.nl
-Date:     2025-11-10
+Date:     2025-11-11
 License:  GNU GPL 3.0
 """
 
 # QBlade verification
-HAWC2_PATH = "verification\\data\\aerodynamics\\HAWC2.csv"          # HAWC2 verification path
-OPENFAST_PATH = "verification\\data\\aerodynamics\\OpenFAST.csv"    # OpenFAST verification path
+BEM_DTU_PATH = "verification\\data\\aerodynamics\\DTU.csv"      # DTU verification path
+BEM_NREL_PATH = "verification\\data\\aerodynamics\\NREL.csv"    # NREL verification path
 
-# Aeroacoustic vertification
-DS_PATH = "verification\\data\\aeroacoustics\\3DS.csv"      # DS verification path
-DLR_PATH = "verification\\data\\aeroacoustics\\DLR.csv"     # DLR verification path
-DTU_PATH = "verification\\data\\aeroacoustics\\DTU.csv"     # DTU verification path
-IAG_PATH = "verification\\data\\aeroacoustics\\IAG.csv"     # IAG verification path
-NREL_PATH = "verification\\data\\aeroacoustics\\NREL.csv"   # NREL verification path
-TNO_PATH = "verification\\data\\aeroacoustics\\TNO.csv"     # TNO verification path
+# Aeroacoustic verification
+CAA_DS_PATH = "verification\\data\\aeroacoustics\\3DS.csv"      # DS verification path
+CAA_DLR_PATH = "verification\\data\\aeroacoustics\\DLR.csv"     # DLR verification path
+CAA_DTU_PATH = "verification\\data\\aeroacoustics\\DTU.csv"     # DTU verification path
+CAA_IAG_PATH = "verification\\data\\aeroacoustics\\IAG.csv"     # IAG verification path
+CAA_NREL_PATH = "verification\\data\\aeroacoustics\\NREL.csv"   # NREL verification path
+CAA_TNO_PATH = "verification\\data\\aeroacoustics\\TNO.csv"     # TNO verification path
